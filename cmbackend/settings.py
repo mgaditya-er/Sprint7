250870dfca826b008cf3846b37493799dc7416bc
mgadity-er <aditya_ghyar@moderncoe.edu.in>
2023-11-21 09:32:57 +0530

    add

--- cmbackend/settings.py
+++ cmbackend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'cmbackend.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj-database-url.config(
+    'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL')
     )
 }
